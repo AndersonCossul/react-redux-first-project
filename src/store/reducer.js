726656255f2ddf1fb951650784b6a4cb3a19d4fd
Anderson Cossul <anderson@dexdigital.com.br>
2018-02-12 21:40:00 -0200

    Mapped actions.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,28 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT_COUNTER':
+      return {
+        ...state,
+        counter: state.counter + 1
+      }
+    case 'DECREMENT_COUNTER':
+      return {
+        ...state,
+        counter: state.counter - 1
+      }
+    case 'ADD_COUNTER':
+      return {
+        ...state,
+        counter: state.counter + action.value
+      }
+    case 'SUBTRACT_COUNTER':
+      return {
+        ...state,
+        counter: state.counter - action.value
+      }
+  }
   return state
 }
 
