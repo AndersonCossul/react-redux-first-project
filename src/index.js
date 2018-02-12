178870d53c507d5a4dfe76d14a83cd202d99c779
Anderson Cossul <anderson@dexdigital.com.br>
2018-02-12 21:13:35 -0200

    Connected Redux store to React.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './store/reducer'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducer)
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 registerServiceWorker()
