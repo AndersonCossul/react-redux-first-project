c6dfcdd8586e8c096153defb19d74906d2577ab8
Anderson Cossul <anderson@dexdigital.com.br>
2018-02-13 00:01:15 -0200

    First steps.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './store/reducer'
+
+const store = createStore(reducer)
+
+const Root = () => {
+  <Provider store={store}>
+    <App />
+  </Provider>
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
