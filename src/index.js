ff617eb527a4611fdb401df1ad798d3d32c57434
Anderson Cossul <anderson@dexdigital.com.br>
2018-02-12 23:58:12 -0200

    Downloaded Project Structure

--- src/index.js
+++ src/index.js
@@ -3,26 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import { createStore, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
-import counterReducer from './store/reducers/counter'
-import resultsReducer from './store/reducers/results'
 
-const rootReducer = combineReducers({
-  counter: counterReducer,
-  results: resultsReducer
-})
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
-
-const Root = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
-
-ReactDOM.render(<Root />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
